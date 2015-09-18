a1a6340b3fff0f30ddf28847d9c28f631e67eef1
Marcel van Kervinck <marcelk@bitpit.net>
2015-09-18 02:02:53 +0200

    Add missing setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup, Extension
+
+module1 = Extension(
+        'floyd',
+        sources = [
+                'Source/floydmodule.c',
+                'Source/format.c',
+                'Source/moves.c',
+                'Source/polyglot.c',
+                'Source/evaluate.c',
+                'Source/kpk.c',
+                'Source/search.c',
+                'Source/cplus.c',
+                'Source/stringCopy.c' ],
+        extra_compile_args = ['-O3', '-std=c99', '-Wall', '-pedantic'],
+        undef_macros = ['NDEBUG']
+)
+
+setup(
+        name         = 'floyd',
+        version      = '1.0a',
+        description  = 'Chess engine study',
+        author       = 'Marcel van Kervinck',
+        author_email = 'marcelk@bitpit.net',
+        url          = 'http://marcelk.net/floyd',
+        ext_modules  = [module1])

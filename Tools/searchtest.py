42ffa2a093829c1be80182eb5521d91945d65143
Marcel van Kervinck <marcelk@bitpit.net>
2015-09-20 23:39:20 +0200

    Add some imbalance tests from start positions

--- Tools/searchtest.py
+++ Tools/searchtest.py
@@ -2,6 +2,23 @@
 import floyd
 
 for pos, depth in [
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -', 0),    # startPosition
+        ('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq -', 0),  # 1. e4
+
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBN1 w Qkq -', 0),     # without Rh1
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/1NBQKBNR w Kkq -', 0),     # without Ra1
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/1NBQKBN1 w kq -', 0),      # without both rooks
+
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKB1R w KQkq -', 0),    # without Ng1
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/R1BQKBNR w KQkq -', 0),    # without Nb1
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/R1BQKB1R w KQkq -', 0),    # without Nb1 Ng1
+
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQK1NR w KQkq -', 0),    # without Bf1
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RN1QKBNR w KQkq -', 0),    # without Bc1
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RN1QK1NR w KQkq -', 0),    # without Bc1 Bf1
+
+        ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNB1KBNR w KQkq -', 0),
+
         ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKB1R w KQkq -', 0),
         ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKB1R w KQkq -', 1),
         ('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKB1R w KQkq -', 2),
